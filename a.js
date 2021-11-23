2fc28da568d319a23a8722a31f0c62ced412b389
abdulhanan9889 <abdul.hanan@emumba.com>
2021-11-24 00:38:20 +0500

    5

--- a.js
+++ a.js
@@ -4,11 +4,4 @@ console.log('modification by lisa')
 console.log('branch diverge practise')
 console.log('third test by lisa');
 console.log('test 5 bug fixed')
-console.log('test 6 added')
-console.log('test 7 added')
-console.log('again');
-console.log('again');
-console.log('again');
-console.log('again');console.log('again');
-console.log('again');console.log('again');
-console.log('again');
+console.log('test 5 added')
