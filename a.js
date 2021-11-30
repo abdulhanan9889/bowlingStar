924bdd0216b22ebce385f65a43bf36e4f8f61974
abdulhanan9889 <abdul.hanan@emumba.com>
2021-11-30 17:45:16 +0500

    resolve

--- a.js
+++ a.js
@@ -3,6 +3,10 @@ console.log('second add');
 console.log('modification by lisa')
 console.log('branch diverge practise')
 console.log('third test by lisa');
+<<<<<<< HEAD
 console.log('commit e');
 console.log('commit f');
 console.log('commit g');
+=======
+console.log('test 5 bug fixed')
+>>>>>>> bf125f8 (test 5 bug  fixed)
